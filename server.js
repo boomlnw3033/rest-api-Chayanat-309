b814d6ec74535254a15c771103c1232fddbc672a
Chayanat Chunmuangphan <boom3033@hotmail.com>
2024-02-16 10:57:04 +0700

    Update-book

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.post('/books', (req, res) => {
   res.status(201).json(req.body)
 })
 
+app.put('/books/:id', (req, res) => {
+  const updateIndex = books.findIndex(book => book.id === req.params.id)
+  res.json(Object.assign(books[updateIndex], req.body))
+})
+
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
