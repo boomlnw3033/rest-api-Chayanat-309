e3b27c0d88c793fd21288b33beb4770f32434bbc
Chayanat Chunmuangphan <boom3033@hotmail.com>
2024-02-16 10:55:42 +0700

    Insert(update)-book

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.get('/books/:id', (req, res) => {
   res.json(books.find(book => book.id === req.params.id))
 })
 
+app.post('/books', (req, res) => {
+  books.push(req.body)
+  res.status(201).json(req.body)
+})
+
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
